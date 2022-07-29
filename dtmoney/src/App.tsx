ed9d7819cb8eafea8e048ff01b025ee94115592e
Vinicius Benso <vinibenso@gmail.com>
2022-07-29 08:48:57 -0300

    header

---
+++ dtmoney/src/App.tsx
@@ -0,0 +1,17 @@
+import { Header } from "./components/Header/Index";
+import { GlobalStyle } from "./Styles/global"
+
+
+
+export function App() {
+  return (
+    <>
+      <Header />
+     <GlobalStyle />
+     
+      
+    </>
+  );
+}
+
+
